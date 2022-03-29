4ee09abcc4aa43f1ac5d12d2a498c31bf104d5e2
Volodymyr Pavlov <shark32@students.zcu.cz>
2022-03-29 13:21:16 +0200

    Global

---
+++ CV7/src/main/java/main/cv7/Animal.java
@@ -0,0 +1,81 @@
+package main.cv7;
+
+import javafx.beans.property.*;
+
+public class Animal {
+
+    private DoubleProperty weight = new SimpleDoubleProperty();
+
+    private StringProperty species = new SimpleStringProperty();
+
+    private StringProperty sound = new SimpleStringProperty();
+
+    private IntegerProperty legsCount = new SimpleIntegerProperty();
+
+    public Animal (){
+
+    }
+
+    public Animal(double weight, String species, String sound, int legsCount) {
+        this();
+
+        setWeight(weight);
+        setSpecies(species);
+        setSound(sound);
+        setLegsCount(legsCount);
+    }
+
+    public double getWeight() {
+        return weight.get();
+    }
+
+    public DoubleProperty weightProperty() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        if (weight>0) {
+            this.weight.set(weight);
+        }else{
+            throw new IllegalArgumentException("Weight must be positive number");
+        }
+    }
+
+    public String getSound() {
+        return sound.get();
+    }
+
+    public StringProperty soundProperty() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound.set(sound);
+    }
+
+    public int getLegsCount() {
+        return legsCount.get();
+    }
+
+    public IntegerProperty legsCountProperty() {
+        return legsCount;
+    }
+
+    public String getSpecies() {
+        System.out.println("Getting species of " + species.get());
+        return species.get();
+    }
+
+    public StringProperty speciesProperty() {
+        System.out.println("Getting property species of " + species.get());
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species.set(species);
+    }
+
+    public void setLegsCount(int legsCount) {
+        this.legsCount.set(legsCount);
+    }
+}
---
+++ CV7/src/main/java/main/cv7/Main.java
@@ -0,0 +1,182 @@
+package main.cv7;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.print.PrinterJob;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.util.StringConverter;
+import javafx.util.converter.DefaultStringConverter;
+import javafx.util.converter.DoubleStringConverter;
+
+import java.text.Collator;
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * main application class
+ *
+ * @author Volodymyr Pavlov
+ * @version 29.03.2022
+ */
+public class Main extends Application {
+
+    /**
+     * Spacing between elements in the boxes
+     */
+    private static final int DEF_SPACING = 10;
+
+    /**
+     * width of the control panel
+     */
+    private static final double CONTROL_WIDTH = 300;
+
+    private Stage primaryStage;
+
+    private ObservableList<Animal> zoo = FXCollections.observableArrayList();
+
+    private TableView<Animal> zooTable;
+
+
+    /**
+     * create main program window
+     *
+     * @param stage -main program window
+     */
+    @Override
+    public void start(Stage stage) {
+        Locale.setDefault(new Locale("cs", "CZ"));
+        createData();
+        stage.setTitle("Animal table");
+        Parent root = createRoot();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(true);
+        primaryStage = stage;
+        stage.show();
+    }
+
+    /**
+     * creating main root panel
+     *
+     * @return root panel
+     */
+    private Parent createRoot() {
+        BorderPane rootPane = new BorderPane();
+
+        rootPane.setCenter(createTablePane());
+        rootPane.setBottom(createStatusBar());
+        return rootPane;
+    }
+
+    private Node createStatusBar() {
+        HBox statusBar = new HBox(DEF_SPACING);
+
+        Button addBtn = new Button("Add random animal");
+        addBtn.setOnAction(e -> addAnimal());
+
+        statusBar.getChildren().add(addBtn);
+        return statusBar;
+    }
+
+    private void addAnimal() {
+        Random r = new Random();
+        Animal animal = new Animal(r.nextDouble(100),"New"," smth",r.nextInt(8));
+
+        zoo.add(animal);
+    }
+
+    private Node createTablePane() {
+        zooTable = new TableView<>();
+
+        zooTable.setItems(zoo);
+        zooTable.setEditable(true);
+
+        TableColumn<Animal, String> speciesColumn = new TableColumn<>("Species");
+        speciesColumn.setCellValueFactory(new PropertyValueFactory<Animal, String>("species"));
+        speciesColumn.setComparator(Collator.getInstance(new Locale("cs", "CZ"))::compare);
+        speciesColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        speciesColumn.setOnEditCommit(e -> {
+            if (e.getNewValue().length() < 5) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.showAndWait();
+                e.getRowValue().setSpecies(e.getOldValue());
+            } else {
+                e.getRowValue().setSpecies(e.getNewValue());
+            }
+            zooTable.refresh();
+        });
+
+        TableColumn<Animal, Double> weightColumn = new TableColumn<>("Weight");
+        weightColumn.setCellValueFactory(new PropertyValueFactory<Animal, Double>("weight"));
+        weightColumn.setCellFactory(TextFieldTableCell.forTableColumn(new StringConverter<Double>() {
+
+            double oldValue;
+
+            @Override
+            public String toString(Double object) {
+                oldValue = object;
+                return object.toString();
+            }
+
+            @Override
+            public Double fromString(String string) {
+                try{
+                    oldValue = Double.parseDouble(string);
+                }catch (NumberFormatException e){
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.showAndWait();
+                }
+                return oldValue;
+            }
+        }));
+
+        TableColumn<Animal, String> soundColumn = new TableColumn<>("Sound");
+        soundColumn.setCellValueFactory(new PropertyValueFactory<Animal, String>("sound"));
+
+        TableColumn<Animal, Integer> legCountColumn = new TableColumn<>("Leg Count");
+        legCountColumn.setCellValueFactory(new PropertyValueFactory<Animal, Integer>("legsCount"));
+
+        zooTable.getColumns().addAll(speciesColumn, weightColumn, soundColumn, legCountColumn);
+        zooTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        zooTable.getSelectionModel().getSelectedItems().addListener((ListChangeListener<? super Animal>) e->{
+
+            showPrintDialog();
+        });
+        return zooTable;
+    }
+
+    private void showPrintDialog() {
+        PrinterJob  job = PrinterJob.createPrinterJob();
+    }
+
+    private void createData() {
+        zoo.add(new Animal(0.05, "Spider", " ", 8));
+        zoo.add(new Animal(0.05, "Mutated spider", "Huuu", 12));
+        zoo.add(new Animal(150, "Zebra", "ihaha", 4));
+        //zoo.add(new Animal(0.2,"Činčila","fffff",4));
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
